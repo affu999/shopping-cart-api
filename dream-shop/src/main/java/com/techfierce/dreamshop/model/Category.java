cc106c7d21a818ccdd4fa6d420196a0707fc18db
affu999 <affanasharief999@gmail.com>
2024-12-17 08:24:09 +0530

    Category model issue resolved

--- dream-shop/src/main/java/com/techfierce/dreamshop/model/Category.java
+++ dream-shop/src/main/java/com/techfierce/dreamshop/model/Category.java
@@ -18,6 +18,6 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
      private List<Product> products;
 }
